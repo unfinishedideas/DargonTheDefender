1096a57782ddfcbd7ec7bd7709de699a503e3623
unfinishedideas <unfinishedideas@gmail.com>
2024-10-14 00:29:25 -0700

    added a level timer to the game (does nothing so far)

--- scenes/Game.cs
+++ scenes/Game.cs
@@ -5,22 +5,27 @@ public partial class Game : Node2D
 {
 	private float _currentTreasure = 999.0f;    // default to 999 so I can see it 
 	private float _maxTreasure = 999.0f;        // change when the signal comes
-
-	private bool _firstTreasureReport = true;
+	private bool _firstTreasureReport = true;   // used to get the initial value of treasure
 
 	// Nodes
 	public RichTextLabel TreasureCounter;
+	public RichTextLabel LevelTimerDisplay;
+	public Timer LevelTimer;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		TreasureCounter = GetNode<RichTextLabel>("%TreasureCounter");
+		LevelTimerDisplay = GetNode<RichTextLabel>("%LevelTimerDisplay");
+		LevelTimer = GetNode<Timer>("%LevelTimer");
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-
+		TimeSpan duration = TimeSpan.FromSeconds(LevelTimer.TimeLeft);
+		LevelTimerDisplay.Text = $"[right]{duration.Minutes}:{duration.Seconds}[/right]";
 	}
 
 	// signals ----------------------------------------------------------------
