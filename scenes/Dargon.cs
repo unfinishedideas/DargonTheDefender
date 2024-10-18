f98aadab11e58593c2578172e9137a896a47aa56
unfinishedideas <unfinishedideas@gmail.com>
2024-10-17 18:37:17 -0700

    added simple sprint / burnout logic for Dargon

--- scenes/Dargon.cs
+++ scenes/Dargon.cs
@@ -13,17 +13,26 @@ public partial class Dargon : CharacterBody2D
 	public float FireRechargeRate = 0.01f;  // how fast fire recharges
 	[Export]
 	public float FireDrainRate = 0.01f;     // how fast fire drains
+	[Export]
+	public float MaxSprintMultiplier = 1.5f; // multiplier to use for sprinting
+	[Export]
+	public float SprintStaminaDrainRate = 0.01f; // host fast stamina drains while sprinting
+	[Export]
+	public float SprintStaminaRechargeRate = 0.01f; // host fast stamina recharges when not sprinting
 
 	private bool _facingRight = true;
 	private bool _isAttacking = false;
 	private bool _burnOut = false;       // used too much fire, must cooldown
 	private float _amountOfFuel = 1.0f;  // 0-1% how much fire left
+	private float _amountOfStamina = 1.0f; // how much stamina left for sprinting
+	private float _currentSprintMultiplier = 1.5f;
 
 	// Nodes
 	public AnimatedSprite2D AnimPlayer;
 	public Area2D AttackArea;
 	public GpuParticles2D AttackParticles;
 	public ProgressBar FireAmmoBar;
+	public ProgressBar StaminaBar;
 	public Timer HurtboxAfterTimer; // how long after the player stops attacking until the hurtbox turns off
 	public AnimationPlayer ColorAnimator;
 
@@ -34,6 +43,7 @@ public partial class Dargon : CharacterBody2D
 		AttackArea = GetNode<Area2D>("%AttackArea");
 		AttackParticles = GetNode<GpuParticles2D>("%AttackParticles");
 		FireAmmoBar = GetNode<ProgressBar>("%FireAmmoBar");
+		StaminaBar = GetNode<ProgressBar>("%StaminaBar");
 		HurtboxAfterTimer = GetNode<Timer>("%HurtboxAfterTimer");
 		HurtboxAfterTimer.Timeout += () => { AttackArea.Monitoring = false; };
 		ColorAnimator = GetNode<AnimationPlayer>("%ColorAnimator");
@@ -52,10 +62,31 @@ public partial class Dargon : CharacterBody2D
 		// Movement related stuff
 		Vector2 velocity = Velocity;
 
+		if (!Input.IsActionPressed("sprint") || _burnOut == true)
+		{
+			_amountOfStamina = Math.Clamp(_amountOfStamina + SprintStaminaRechargeRate, 0, 1.0f);
+		}
+
 		// Get the input direction and handle the movement/deceleration.
 		Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
 		if (direction != Vector2.Zero)
 		{
+			// Handle Sprinting Speed
+			if (Input.IsActionPressed("sprint") && _burnOut == false && _amountOfStamina > 0)
+			{
+				_currentSprintMultiplier = MaxSprintMultiplier;
+				_amountOfStamina -= SprintStaminaDrainRate;
+				if (_amountOfStamina <= 0)
+				{
+					EnterBurnout();
+				}
+			}
+			else
+			{
+				_currentSprintMultiplier = 1.0f;
+			}
+
+			// Handle Regular Speed
 			if (!_isAttacking)
 			{
 				AnimPlayer.Play("walk");
@@ -73,8 +104,8 @@ public partial class Dargon : CharacterBody2D
 			{
 				currentSpeed = Speed;
 			}
-			velocity.X = direction.X * currentSpeed;
-			velocity.Y = direction.Y * currentSpeed;
+			velocity.X = direction.X * currentSpeed * _currentSprintMultiplier;
+			velocity.Y = direction.Y * currentSpeed * _currentSprintMultiplier;
 		}
 		else
 		{
@@ -96,15 +127,15 @@ public partial class Dargon : CharacterBody2D
 	private void UpdatePlayerFacing(float velocityX)
 	{
 		// Check facing and flip sprite and attack area location
-		_facingRight = velocityX switch 
+		_facingRight = velocityX switch
 		{
 			> 0 => true,
 			< 0 => false,
-			0   => _facingRight,
-			_   => _facingRight,
+			0 => _facingRight,
+			_ => _facingRight,
 		};
 		AnimPlayer.FlipH = !_facingRight;
-		switch(_facingRight)
+		switch (_facingRight)
 		{
 			case true:
 				AttackArea.Scale = new Vector2(1, AttackArea.Scale.Y);
@@ -122,7 +153,7 @@ public partial class Dargon : CharacterBody2D
 	{
 		ParticleProcessMaterial partyMaterial = (ParticleProcessMaterial)AttackParticles.ProcessMaterial;
 
-		Vector3 newEmissionShapeOffset = partyMaterial.EmissionShapeOffset; 
+		Vector3 newEmissionShapeOffset = partyMaterial.EmissionShapeOffset;
 		newEmissionShapeOffset.X = _facingRight ? 20 : -20;
 
 		Vector3 newEmissionDirection = partyMaterial.Direction;
@@ -147,8 +178,7 @@ public partial class Dargon : CharacterBody2D
 			_amountOfFuel = Math.Clamp(_amountOfFuel - FireDrainRate, 0, 1);
 			if (_amountOfFuel <= 0)
 			{
-				_burnOut = true;
-				ColorAnimator.Play("burnout");
+				EnterBurnout();
 			}
 			else
 			{
@@ -168,7 +198,7 @@ public partial class Dargon : CharacterBody2D
 			_isAttacking = false;
 			AttackParticles.Emitting = false;
 			_amountOfFuel = Math.Clamp(_amountOfFuel + FireRechargeRate, 0, 1);
-			if(_amountOfFuel == 1)
+			if (_amountOfFuel == 1 && _amountOfStamina == 1)
 			{
 				_burnOut = false;
 				ColorAnimator.Play("RESET");
@@ -179,7 +209,17 @@ public partial class Dargon : CharacterBody2D
 	// Update the Player UI (health, fuel etc) --------------------------------
 	private void UpdateUI(double delta)
 	{
-		// Refresh Ammo bar graphic
+		// Refresh Stamina bar UI
+		if (_amountOfStamina < 1)
+		{
+			StaminaBar.Visible = true;
+			StaminaBar.Value = _amountOfStamina;
+		}
+		else
+		{
+			StaminaBar.Visible = false;
+		}
+		// Refresh Ammo bar UI
 		if (_amountOfFuel < 1)
 		{
 			FireAmmoBar.Visible = true;
@@ -189,5 +229,12 @@ public partial class Dargon : CharacterBody2D
 		{
 			FireAmmoBar.Visible = false;
 		}
+	}
+
+	// Dargon enters the burnout state ----------------------------------------
+	private void EnterBurnout()
+	{
+		_burnOut = true;
+		ColorAnimator.Play("burnout");
 	}
 }
